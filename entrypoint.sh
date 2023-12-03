bf37aa7fad7a2c33af5e36abb61381c02bbe381b
znnznn <znnznn@ukr.net>
2023-12-04 00:06:56 +0200

    fix deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ echo "Starting migrate..."
 alembic upgrade head
 
 echo "Starting server..."
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+uvicorn main:app --host 0.0.0.0 --port 80 --reload
 
 exec "$@"
